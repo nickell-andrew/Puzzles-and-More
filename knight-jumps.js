782dae9bd70e3747fa91a88cfde0b515e2312a1f
Andrew Nickell <nickell.andrew@gmail.com>
2015-11-16 15:22:18 -0800

    add knight-jumps.js

---
+++ knight-jumps.js
@@ -0,0 +1,43 @@
+//Using the JavaScript language, have the function KnightJumps(str) 
+//read str which will be a string consisting of the location of a knight 
+//on a standard 8x8 chess board with no other pieces on the board. The 
+//structure of str will be the following: "(x y)" which represents the 
+//position of the knight with x and y ranging from 1 to 8. Your program 
+//should determine the number of spaces the knight can move to from a 
+//given location. For example: if str is "(4 5)" then your program should 
+//output 8 because the knight can move to 8 different spaces from 
+//position x=4 and y=5. 
+
+// Advanced challenges are worth 15 points and you are not timed for 
+//them. Use the Parameter Testing feature in the box below to test your 
+//code with different arguments.
+
+function KnightJumps(str) { 
+
+  // code goes here  
+  return str; 
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+KnightJumps(readline());                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
