3cd0f1982f3563c3213f04060d75dfd1588108d7
Andrew Nickell <nickell.andrew@gmail.com>
2015-11-30 14:19:45 -0800

    add hexmaze and wordpattern

---
+++ hexmaze.js
@@ -0,0 +1,11 @@
+/* you and a coworker are building a game that is played on a near infinite hex grid
+
+She is going to design the layout of the grid and she'll supplies you with a function
+that determines whether any hex space is passable or impassable
+
+You must find the shortest path between two given hex points, passing only through
+passable locations
+
+Inputs: 2 location in a plane (hex grid)  / grid / function that checks passability (memory look up (constant time))
+
+analyze Time and Space Complexity and (optimize if possible) */
